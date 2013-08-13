d4a8833b6218b2160ac0d25c0b7598b2cfb20433
Barry Allard <barry.allard@gmail.com>
2013-08-13 07:22:26 -0700

    cleanup

--- Makefile
+++
@@ -1,7 +0,0 @@
-all: libkeccak.a
-
-libkeccak.a: KeccakSponge.o KeccakF-1600-opt64.o KeccakNISTInterface.o
-	ar -rs $@ $<
-
-clean:
-	rm -f *.a *.o
--- keccak.go
+++ keccak.go
@@ -9,18 +9,15 @@ import (
 )
 
 type keccak struct {
-    hashState C.hashState
-    bitlen C.int
-    DataLength C.DataLength
+    hashState  C.hashState
+    bitlen     C.int
+    dataLength C.DataLength
 }
 
 func newKeccak(bitlen int) hash.Hash {
-    var k keccak
-    k.bitlen = C.int(bitlen)
-    if C.Init(&k.hashState, k.bitlen) == C.BAD_HASHLEN {
-        panic("bad hashlen")
-    }
-    return &k
+    k := &keccak{bitlen: C.int(bitlen)}
+    k.Reset()
+    return k
 }
 
 func New224() hash.Hash {
@@ -41,10 +38,10 @@ func New512() hash.Hash {
 
 func (k *keccak) Write(b []byte) (int, error) {
     n := len(b)
-    dl := C.DataLength(n * 8)
     if n == 0 {
         return 0, nil
     }
+    dl := C.DataLength(n*8)
     p := (*C.BitSequence)(unsafe.Pointer(&b[0]))
     if C.Update(&k.hashState, p, dl) != C.SUCCESS {
         return 0, errors.New("keccak write error")
@@ -52,34 +49,24 @@ func (k *keccak) Write(b []byte) (int, error) {
     return n, nil
 }
 
-func (k *keccak) BlockSize() int {
-    switch int(k.bitlen) {
-    case 224: return 1152/8
-    case 256: return 1088/8
-    case 384: return  832/8
-    case 512: return  576/8
-    default:  return    1
+func (k *keccak) Sum(b []byte) []byte {
+    k0 := *k
+    buf := make([]byte, k.Size(), k.Size())
+    p := (*C.BitSequence)(unsafe.Pointer(&buf[0]))
+    if C.Final(&k0.hashState, p) != C.SUCCESS {
+        panic("keccak sum error")
     }
+    return append(b, buf...)
 }
 
 func (k *keccak) Reset() {
-    if C.Init(&k.hashState, k.bitlen) != C.BAD_HASHLEN {
-        panic("bad hashlen")
-    }
+    C.Init(&k.hashState, k.bitlen)
 }
 
-func (k *keccak) Size() int {
+func (k keccak) BlockSize() int {
+    return 200 - 2*k.Size()
+}
+
+func (k keccak) Size() int {
     return int(k.bitlen) / 8
-}
-
-func (k *keccak) Sum(b []byte) []byte {
-    k0 := *k
-
-    buf := make([]byte, k.Size(), k.Size())
-
-    if C.Final(&k0.hashState, (*C.BitSequence)(unsafe.Pointer(&buf[0]))) != C.SUCCESS {
-        panic("keccak sum error")
-    }
-
-    return append(b, buf...)
 }
